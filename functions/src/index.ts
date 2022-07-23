61dd7efe2ca37a264542958a45d64bd75cb75077
JaeHo Song <thruthesky@gmail.com>
2022-07-23 22:17:50 +0900

    deploy post functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,6 @@ admin.firestore().settings({ignoreUndefinedProperties: true});
 
 export * from "./indexes/user.functions";
 export * from "./indexes/category.functions";
-// export * from "./indexes/post.functions";
+export * from "./indexes/post.functions";
 // export * from "./indexes/storage.functions";
 // export * from "./indexes/messaging.functions";
